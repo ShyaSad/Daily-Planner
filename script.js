bc4ab843a8039dc01cce9ce8b0b8fe2898adfe6f
shyasadeghi@gmail.com <shyasadeghi@gmail.com>
2020-02-03 15:02:31 -0800

    changes made for TA

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ function storeData(event) {
     localStorage.setItem("#btn1", text);
 };
 
-$("#btn1").on("click", function() {
+$(".btnPos").on("click", function() {
+    let current = $(this)
+    console.log(current)
     console.log("was clicked");
+    for (let i = 0; i < 12; i++){
     let currentInfo = $(".form-control").val();
     console.log(currentInfo);
-    localStorage.setItem("form submission",currentInfo);
+    localStorage.setItem("form-submission",currentInfo);}
+    loadData();
 
 });
 
+
 function loadData() {
     console.log("dataloaded");
-    let gotData = localStorage.getItem("form submission");
-    $(".form-control").val() = gotData;
+    let gotData = localStorage.getItem("form-submission");
+    console.log($(this))
+    $(this).val(gotData);
 };
-
-loadData();
